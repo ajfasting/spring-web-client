b5df143b0433f7637591c7700d8b7f7d8b574281
Scott Hopfensperger <shopfensperger@brookdale.com>
2020-05-13 21:48:58 -0500

    Helping the wizard

--- src/main/java/com/nm/springwebclient/controller/SearchController.java
+++ src/main/java/com/nm/springwebclient/controller/SearchController.java
@@ -1,25 +1,32 @@
 package com.nm.springwebclient.controller;
 
-import com.nm.springwebclient.client.SearchClient;
-import com.nm.springwebclient.model.SearchProperties;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nm.springwebclient.client.SearchClient;
+import com.nm.springwebclient.model.Brewery;
+
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RestController
 public class SearchController {
 
     @Autowired
     private SearchClient searchClient;
-
-
-
+    
+    @Autowired
+    private Environment environment;
+    
     @GetMapping("/breweries")
-    public Flux<SearchProperties> listBreweries() {
+    public Flux<Brewery> listBreweries() {
+    	log.info(environment.getProperty("breweries"));
         return searchClient.listBreweries();
     }
+    
+    // Add more enpoints for search and get brewery
     
 }
---
+++ src/main/java/com/nm/springwebclient/model/Brewery.java
@@ -0,0 +1,59 @@
+package com.nm.springwebclient.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Brewery {
+    
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("brewery_type")
+    private String breweryType;
+
+    @JsonProperty("street")
+    private String street;
+
+    @JsonProperty("city")
+    private String city;
+
+    @JsonProperty("state")
+    private String state;
+
+    @JsonProperty("postal_code")
+    private String postalCode;
+
+    @JsonProperty("country")
+    private String country;
+
+    @JsonProperty("longitude")
+    private BigDecimal longitude;
+
+    @JsonProperty("latitude")
+    private BigDecimal latitude;
+
+    @JsonProperty("phone")
+    private String phone;
+
+    @JsonProperty("website_url")
+    private String websiteUrl;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+
+    @JsonProperty("tag_list")
+    private List<String> tagList;
+}
