babffcc96376c17c1ac3fa5c964d88e95c67af82
Scott Hopfensperger <shopfensperger@brookdale.com>
2020-05-14 22:59:28 -0500

    Global Errors

---
+++ src/main/java/com/nm/spirngwebclient/controller/model/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.nm.spirngwebclient.controller.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ErrorResponse implements Serializable {
+	private static final long serialVersionUID = 4145313648759396395L;
+	
+	private LocalDateTime timestamp;
+	private String status;
+	private String error;
+	private String message;
+	private String path;
+}
